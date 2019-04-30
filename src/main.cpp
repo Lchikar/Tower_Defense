32a0fddeeadb3fd731ba6655e5b7491041d9dc97
Amandine <akohlmuller@gmail.com>
2019-04-30 14:54:45 +0200

    main include

--- src/main.cpp
+++ src/main.cpp
@@ -5,3 +5,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+
+#include "../include/Entity.hpp"
+#include "../include/Map.hpp"
+#include "../include/Building.hpp"
+#include "../include/Tower.hpp"
+#include "../include/Alien.hpp"
+#include "../include/Path.hpp"
+#include "../include/Position.hpp"
