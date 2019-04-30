6681ff4f67c7b503f34cda15c20a7a75e225cc6c
Amandine <akohlmuller@gmail.com>
2019-04-30 16:11:34 +0200

    entity get set

--- src/Entity.cpp
+++ src/Entity.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <string>
 
+#include "Position.hpp"
+#include "Entity.hpp"
+
+Entity();
+
+~Entity();
+		
+Position getPos() {
+	Position pos = this.pos;
+	return pos;
+}
+
+void setPos(Position p) {
+	this.pos = p;
+}
+
+string getSprite() {
+	string sprite = this.sprite;
+	return sprite;
+}
+
+void setSprite(string sprite) {
+	this.sprite = sprite;
+}
