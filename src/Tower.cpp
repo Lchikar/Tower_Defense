10e95c2f9d30724fa957932467a47593a3f65d16
Amandine <akohlmuller@gmail.com>
2019-04-30 16:44:10 +0200

    get set Tower

--- src/Tower.cpp
+++ src/Tower.cpp
@@ -3,3 +3,53 @@
 
 #include "Entity.hpp"
 #include "Tower.hpp"
+
+Tower();
+
+int getDamage() {
+	int damage = this->damage;
+	return damage;
+}
+
+int getRange() {
+	int range = this->range;
+	return range;
+}
+
+int getShotRate() {
+	int shotRate = this->shotRate;
+	return shotRate;
+}
+
+int getPrice() {
+	int price = this->price;
+	return price;
+}
+
+string getColor() {
+	string color = this->color;
+	return color;
+}
+
+void setDamage(int damage) {
+	this->damage = damage;
+}
+
+void setRange(int range) {
+	this->range = range;
+}
+
+void setShotRate(int shotRate) {
+	this->shotRate = shotRate;
+}
+
+void setPrice(int price) {
+	this->price = price;
+}
+
+void setColor(string color) {
+	this->color = color;
+}		
+
+bool isBuildable(Position pos); /* vérifie si la tour peut etre construite a la position 'pos' */
+Position target(); /* renvoie la position de l'alien le plus proche */
