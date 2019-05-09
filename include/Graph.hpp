d3c5caeaa02be67f887f67dc520ee3169a6aca61
Louisa CHIKAR <louisa.chikar@gmail.com>
2019-05-09 16:50:20 +0200

    Graph.hpp

---
+++ include/Graph.hpp
@@ -0,0 +1,21 @@
+#ifndef DEF_GRAPH
+#define DEF_GRAPH
+ 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Graph {
+	
+	private:
+
+	public:
+		Graph();
+		~Graph();
+		
+		
+		
+};
+
+#endif
